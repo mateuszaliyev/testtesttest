384a2c6385f18d51f814a5531d3ebb4a1fcdbf62
patrykstk <code.patrykstasik@gmail.com>
2024-10-31 13:36:02 +0100

    fix: missing login-schema source in the authorize function

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,6 @@
 import type { NextAuthConfig } from "next-auth"
 import Credentials from "next-auth/providers/credentials";
-import { LoginSchema } from "@/schemas";
+import { LoginSchema } from "./schemas/login-schema";
 import { getUserByEmail } from "@/data/user";
 
 import bcrypt from "bcryptjs";
